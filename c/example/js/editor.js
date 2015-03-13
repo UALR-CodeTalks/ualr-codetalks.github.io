cf58526f6e3b2b91eacf4d802099c8df9ac90bae
Alex-Jaeger <agjaeger@ualr.edu>
2015-03-13 11:24:27 -0500

    Added Ace

---
+++ c/example/js/editor.js
@@ -0,0 +1,17 @@
+
+var editor = ace.edit("editor");
+editor.setTheme("ace/theme/monokai");
+editor.getSession().setMode("ace/mode/javascript");
+editor.resize();
+
+function changeTheme(selectTheme) {
+    var value = selectTheme.options[selectTheme.selectedIndex].value;
+
+    editor.setTheme("ace/theme/"+value);
+}
+
+function changeMode(selectMode) {
+    var value = selectMode.options[selectMode.selectedIndex].value;
+
+    editor.getSession().setMode("ace/mode/"+value);
+}
